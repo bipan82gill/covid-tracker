d5bfa3d1dd808b122834d41c586ca1e6f14b1d2f
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-06-15 23:23:41 -0400

    adding components and run app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { Cards, Chart, CountryPicker } from './Components';
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>APP</h1>
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
