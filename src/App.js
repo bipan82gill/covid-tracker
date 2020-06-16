7a37518c09311dae396d1a2595d40d9ee60a90a8
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-06-16 17:41:06 -0400

    axios call to fetch updated data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './Components';
+import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
+
+    async componentDidMount(){
+        const data = await fetchData();
+        console.log(data);
+        }
+    
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <Cards />
                 <Chart />
                 <CountryPicker />
