b9c2a15dc49ae01b52faaff175cfcddea8cffbb3
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-06-15 23:03:58 -0400

    setup App and index.js files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>APP</h1>
+            </div>
+        )
+    }
 }
 
 export default App;
