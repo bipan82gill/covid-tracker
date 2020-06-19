d30eea6b28541ba815dc28a23eee27d103d94bcc
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-06-18 22:57:34 -0400

    add line chat for daily data

--- src/api/index.js
+++ src/api/index.js
@@ -6,9 +6,25 @@ export const fetchData = async () => {
     try{
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
        
-        const modifiedData = { confirmed, recovered, deaths, lastUpdate }
-        return modifiedData;
+        return  { confirmed, recovered, deaths, lastUpdate }
+       
     }catch (error){
 
     }
 }
+ export const fetchDailyData = async () =>{
+     try{
+         const { data } = await axios.get(`${url}/daily`);
+
+         const modifiedData = data.map((dailyData) => ({
+             confirmed: dailyData.confirmed.total,
+             deaths: dailyData.deaths.total,
+             date: dailyData.reportDate,
+         }));
+       return modifiedData;
+     }
+     catch (error){
+         console.log(error);
+
+     }
+ }
