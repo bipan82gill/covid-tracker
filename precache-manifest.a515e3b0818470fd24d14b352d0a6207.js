0cd0150d99bec8f4481a4c49ec0896867e813bdf
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-06-20 18:50:07 -0400

    Updates

---
+++ precache-manifest.a515e3b0818470fd24d14b352d0a6207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5e0d0ae0294cf2fa941df6bed9844a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "7227924384344de311b2",
+    "url": "/covid-tracker/static/css/main.a26d6318.chunk.css"
+  },
+  {
+    "revision": "de35e49e6104c85b8680",
+    "url": "/covid-tracker/static/js/2.3b2fe09a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.3b2fe09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7227924384344de311b2",
+    "url": "/covid-tracker/static/js/main.9d7c6be2.chunk.js"
+  },
+  {
+    "revision": "4ab6cc6a6673a41c1175",
+    "url": "/covid-tracker/static/js/runtime-main.51b14c82.js"
+  },
+  {
+    "revision": "7786bbd41b0ab3b8fdc2d472789e306e",
+    "url": "/covid-tracker/static/media/COVID-19.7786bbd4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-tracker/precache-manifest.cee4a66141d9a09f40720cf1fd5d11a6.js"
+  "/covid-tracker/precache-manifest.a515e3b0818470fd24d14b352d0a6207.js"
 );
 
 self.addEventListener('message', (event) => {
